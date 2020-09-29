ebe595b1dcbdcfcbd7068ad20ffd3b0e6533ad89
dianakoh <dianakoh94@gmail.com>
2020-09-29 20:16:23 +0900

    setting spring boot and ionic

---
+++ src/main/java/com/devflowing/Board_Ionic/BoardIonicApplication.java
@@ -0,0 +1,13 @@
+package com.devflowing.Board_Ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devflowing/Board_Ionic/BoardIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devflowing.Board_Ionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
